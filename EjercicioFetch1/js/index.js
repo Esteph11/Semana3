ee4636086fc7ed5398b32d14e359a04b04186eb5
Esteph11 <esteportilloluna11@gmail.com>
2024-11-05 07:44:43 -0600

    EjercicioFetch1
    Lista de usuarios

--- EjercicioFetch1/js/index.js
+++ EjercicioFetch1/js/index.js
@@ -1,3 +1,26 @@
-function calcular() {
-    // Obtener las notas  
-}
+// Obtener la lista de usuarios  
+fetch('https://api.escuelajs.co/api/v1/users') 
+.then(response => response.json()) //Conversion a JSON
+.then(data => {
+let cards = ''; 
+    for (let user of data) {  
+
+        let avatar = `https://i.pravatar.cc/150?img=${user.id}`; // Generar un avatar basado en el id del usuario  
+        
+        cards += `<div class="col-md-2 mb-2">  
+                    <div class="card">  
+                        <img src="${avatar}" class="card-img-top" alt="Avatar">  
+                        <div class="card-body">  
+                            <h5 class="card-title">${user.name}</h5>  
+                            <p class="card-text"><strong>Email:</strong> ${user.email}</p>  
+                             <p class="card-text"><strong>Contraseña:</strong> ${user.password}</p> 
+                        </div>  
+                    </div>  
+                  </div>`;  
+    }  
+
+    // Renderizar las tarjetas a HTML  
+    document.getElementById("cardsList").innerHTML = cards;  
+})  
+
+
